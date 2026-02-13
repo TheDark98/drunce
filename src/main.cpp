82f15c043577cc15c4ebaa24e9ef87293b289b4c
TheDark98 <dark98cooperation@gmail.com>
2026-02-13 16:47:55 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,129 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <cctype>
+#include <random>
 
-#include "seedGen.h"
-#include "cards.h"
+#include <engine.h>
+#include <card.h>
+#include <deck.h>
+#include <hand.h>
 
-#define MAX_PLAYED_ROUNDS 9
+#define MAX_ROUNDS 9
 
-unsigned int askPlayerSeed() {
-    printf("Want a random seed? (Y/n): ");
-    std::string response;
-    std::getline(std::cin, response);
-    response.erase(std::remove_if(response.begin(), response.end(), isspace), response.end());
-
-    if (response[0] != 'n' && response[0] != 'N') return 0;
-
-    printf("Seed (0 is random): ");
-    std::string seedResponse;
-    std::getline(std::cin, seedResponse);
-    seedResponse.erase(std::remove_if(response.begin(), response.end(), isdigit), response.end());
-    unsigned int playerSeed;
-    if (std::floor(playerSeed) == playerSeed)
-        return playerSeed;
-    return 0;
-}
-
-std::pair<cards::value, cards::seed> generateCard() {
-    const unsigned int generatedValue = seedBlackJack::getRandom();
-
-    cards::seed generatedCardSeed = (cards::seed) (generatedValue % 4 + 1);
-    cards::value generatedCardValue = (cards::value) (generatedValue % 13 + 1);
-
-    return std::make_pair(generatedCardValue, generatedCardSeed);
-}
+std::string InputLine();
+bool InputIsYes(const std::string_view response);
+uint64_t askPlayerSeed();
+bool isPlayerWinner(const DrunkEngine::Hand &playerHand, const DrunkEngine::Hand &dealerHand);
 
 int main() {
-    bool isRunnning = true;
+    DrunkEngine::GameEngine engine;
+    while (!engine.IsInGameState(DrunkEngine::GameState::EXIT)) {
+        const uint64_t seed = askPlayerSeed();
+        engine.SetGameState(DrunkEngine::GameState::PLAYING);
 
-    while (isRunnning) {
-        const unsigned int seed = seedBlackJack::generate(askPlayerSeed());
-        bool isGaming = true;
+        while (engine.IsInGameState(DrunkEngine::GameState::PLAYING) || engine.IsInGameState(DrunkEngine::GameState::PAUSED)) {
+            DrunkEngine::Deck deck(seed);
+            
+            DrunkEngine::Hand playerHand;
+            DrunkEngine::Hand dealerHand;
 
-        while (isGaming) {
+            playerHand.AddCard(deck.Draw());
+            printf("You got: %s\n", DrunkEngine::FromCardToString(deck.LastDraw()).data());
+            dealerHand.AddCard(deck.Draw());
+            printf("Dealer got: %s\n", DrunkEngine::FromCardToString(deck.LastDraw()).data());
+            
+            playerHand.AddCard(deck.Draw());
+            printf("You got: %s\n", DrunkEngine::FromCardToString(deck.LastDraw()).data());
+            dealerHand.AddCard(deck.Draw());
+            printf("Dealer got a card\n");
 
-            std::pair<cards::value, cards::seed> dealerHand[11] = {std::make_pair(cards::value::VNull, cards::seed::SNull)};
-            std::pair<cards::value, cards::seed> playerHand[11] = {std::make_pair(cards::value::VNull, cards::seed::SNull)};
-
-            playerHand[0] = generateCard();
-            playerHand[1] = generateCard();
-
-            dealerHand[0] = generateCard();
-            dealerHand[1] = generateCard();
+            printf("Your Hand Value: %d\n", playerHand.GetValue());
 
             // Player Draw Phase
-            for (unsigned int roundNumber; roundNumber < MAX_PLAYED_ROUNDS; roundNumber++) {
+            for (uint8_t i = 0; i < MAX_ROUNDS; i++) {
                 printf("Draw a card? (Y/n): ");
-                std::string response;
-                std::getline(std::cin, response);
-                response.erase(std::remove_if(response.begin(), response.end(), isspace), response.end());
+                if (!InputIsYes(InputLine()) || playerHand.GetValue() > 21) break;
 
-                if (response[0] == 'n' || response[0] == 'N') break;
-
-                playerHand[roundNumber + 2] = generateCard();
+                playerHand.AddCard(deck.Draw());
+                printf("You got: %s\n", DrunkEngine::FromCardToString(deck.LastDraw()).data());
+                printf("Your Hand Value: %d\n", playerHand.GetValue());
             }
             
             // Dealer Draw Phase
-            for (unsigned int roundNumber; roundNumber < MAX_PLAYED_ROUNDS; roundNumber++) {
+            for (uint8_t i = 0; i < MAX_ROUNDS; i++) {
+                if (playerHand.GetValue() > 21) break;
+                if (dealerHand.GetValue() > 21) break;
+
                 std::random_device rd;
-                
+
                 bool botStopDrawing = (rd() % 2);
-                
+
                 if (botStopDrawing) break;
 
-                dealerHand[roundNumber + 2] = generateCard();
+                dealerHand.AddCard(deck.Draw());
+                printf("Dealer got a card\n");
             }
-            const unsigned int playerHandValue = handValue(playerHand);
-            const unsigned int dealerHandValue = handValue(dealerHand);
 
-            if (playerHandValue != 0)
-                printf("You Win!\n");
-            else if (dealerHandValue != 0)
-                printf("You Loose!\n");
+            if (playerHand.GetValue() < 22) {
+                printf("Your Hand\n");
+                printf("%s\n", DrunkEngine::FromCardToString(playerHand.GetHand()).data());
+                printf("Value of Your Hand: %d\n", playerHand.GetValue());
+
+                printf("Dealer Hand\n");
+                printf("%s\n", DrunkEngine::FromCardToString(dealerHand.GetHand()).data());
+                printf("Value of Dealer Hand: %d\n", dealerHand.GetValue());
+            }
+            else printf("Oof... You broke the barrier");
+
+            bool playerWon = isPlayerWinner(playerHand, dealerHand);
+
+            if (playerWon)
+                engine.SetGameState(DrunkEngine::GameState::WIN);
             else
-                printf("Pair!\n");
-
-            isGaming = false; 
+                engine.SetGameState(DrunkEngine::GameState::LOSE);
         }
+        printf("You %s\n", engine.IsInGameState(DrunkEngine::GameState::WIN) ? "WON!" : "LOSE!");
         printf("Play Again? (Y/n): ");
-        std::string response;
-        std::getline(std::cin, response);
-        response.erase(std::remove_if(response.begin(), response.end(), isspace), response.end());
-        
-        if (response[0] == 'n' || response[0] == 'N') isRunnning = false;
+        if (!InputIsYes(InputLine())) engine.SetGameState(DrunkEngine::GameState::EXIT);
     }
     return 0;
+}
+
+std::string InputLine() {
+    std::string response;
+    std::getline(std::cin, response);
+    response.erase(std::remove_if(response.begin(), response.end(), isspace), response.end());
+    return response;
+}
+
+bool InputIsYes(const std::string_view response) {
+    const char lowerChar = response.empty() ? 'f' : std::tolower(response[0]);
+    
+    if (lowerChar == 'n' || lowerChar == 'm' || lowerChar == 'b') return false;
+    else if (lowerChar == 'y' || lowerChar == 't' || lowerChar == 'u') return true;
+
+    return true;
+}
+
+uint64_t askPlayerSeed() {
+    printf("Seed (0 is random): ");
+    std::string userResponse = InputLine();
+    if (userResponse.empty()) userResponse = "0";
+    uint64_t response = std::stoull(userResponse);
+
+    if (response != 0) return response;
+    std::random_device rd;
+    uint64_t doublemized = static_cast<uint64_t>(rd()) << 32;
+    doublemized += static_cast<uint64_t>(rd());
+    return doublemized;
+}
+
+bool isPlayerWinner(const DrunkEngine::Hand &playerHand, const DrunkEngine::Hand &dealerHand) {
+    bool playerIsGreater = playerHand.GetValue() > dealerHand.GetValue();
+    bool playerHandIsValid = playerHand.GetValue() < 22;
+    bool dealerHandIsValid = dealerHand.GetValue() < 22;
+
+    if (playerHandIsValid && dealerHandIsValid) return playerIsGreater;
+    if (!playerHandIsValid) return false;
+    return true;
 }
